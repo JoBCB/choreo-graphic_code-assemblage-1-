90817a2f1e5068752dfcfb89707001d217ac6326
JoChicau <JoBCB@users.noreply.github.com>
2018-05-10 12:43:27 +0100

    new update

--- choreographic_code_altogether.js
+++ choreographic_code_altogether.js
@@ -8,7 +8,7 @@
 //GLOSSARY
 
 
-// ACT 1  <title>This is a Neutral Performance</title>
+// <title> Act 1 </title>
 
 
 function Warning() {
@@ -66,15 +66,15 @@ Texts[7]="as it seems to be.";
 
 
 function New_Sequence_or_Phrasing(){
-document.querySelector('#logo').innerHTML=Texts[count];
+document.querySelector('#logocont').innerHTML=Texts[count];
 count++;
 if(count==Texts.length){count='0';}
 setTimeout("New_Sequence_or_Phrasing()",delay*1000);
 
 
 document.querySelector("#logocont").style.width="1000px"
-document.querySelector("#logo").style.width="1000px"
-document.querySelector("#logo").style.fontSize="24pt"
+document.querySelector("#logocont").style.width="1000px"
+document.querySelector("#logocont").style.fontSize="24pt"
 }
 
 //onload= function() { Sequencing();}
@@ -126,7 +126,7 @@ function  Indeterminancy(n) {
 var rannum= Math.floor(Math.random()*textarray3.length);
 // document.querySelector(".st").innerHTML=textarray2[rannum];
 var r =document.querySelector(".g:nth-child("+n+")"),
-  _Rm = r.querySelector("._Rm");
+  _Rm = r.querySelector(".iUh30");
 _Rm.innerHTML=textarray3[rannum];
 _Rm.style.fontSize="16t"
 };	
@@ -170,7 +170,7 @@ st.style.marginTop = "40px";
 
 //onload = function() { Elements_of_Chance();}
 
-//document.querySelector(“#main”).innerHTML=""
+//document.querySelector(â€œ#mainâ€).innerHTML=""
 
 function leave_the_stage (n){
 
@@ -207,10 +207,64 @@ function Second_Act () {
   window.location.href = url;
   url.focus();
 }
-// document.querySelector(“html”).style.backgroundColor = "white"
+// document.querySelector(â€œhtmlâ€).style.backgroundColor = "white"
 
 
-// ACT 2  <title>Spacing and Timing</title>
+// <title>Act 2</title>
+
+
+ // - -  - - - - - -  - - - - -- - - - - - - - - - - - - - - - -   on google images:
+
+
+colors = new Array(" #000000 ", "#101010 ","#202020 ", "#282828 ","#303030" ,"#383838  ", "#404040 ", "#484848 ","#585858 " ,"#505050 ", "#606060 ","#686868 ", "#787878 ",  "#888888 ", "#989898 ", "#A0A0A0 ", "#A8A8A8 ","#C0C0C0 ", "#B0B0B0 ", "#F0F0F0 "," #D8D8D8 ", "#F0F0F0 ","#FFFFFF ","#D0D0D0", " #181818 ", "#909090 ")
+
+colorIndex = 0
+idInterval = 0
+
+function choreography_is_the_organization_of_tensions() {
+ document.querySelector("body").style.backgroundColor = colors[colorIndex]
+ colorIndex = (colorIndex + 1) % colors.length
+}
+//setInterval("choreography_is_the_organization_of_tensions()",20)
+
+
+
+var delay="10"; //how many seconds
+var count='0';
+var Texts=new Array();
+Texts[0]="Counterclockwise";
+Texts[1]="Sphere";
+Texts[2]="Spheres";
+Texts[4]="";
+Texts[5]="The private circle";
+Texts[6]="Public sphere";
+Texts[7]="The private circle";
+Texts[8]="";
+Texts[9]="Rotation";
+Texts[10]="";
+Texts[11]="Spinning";
+Texts[12]="Spinning wheel";
+Texts[13]="Perpetual motion";
+Texts[14]="";
+Texts[15]="Repeating itself ";
+Texts[16]="Geometry of time";
+
+    function perpetual_motion(){
+        document.querySelector('#lst-ib').value = Texts[count]; //or: #searchboxinput
+        count++;
+        if(count==Texts.length){count='0';}
+        setTimeout("perpetual_motion()",delay*800);
+    } 
+    // setInterval("perpetual_motion()",1600)
+
+ // - -  - - - - - -  - - - - -- - - - - - - - - - - - - - - - -   
+
+function Thank_You () {
+  var win = window.open('curtain_down.html',"_self");
+  win.focus();
+}
+
+
 
 /*document.querySelector("#main").innerHTML="";
 
@@ -399,10 +453,109 @@ window.open("", "", "width=2000, height=100");
 }
 
 
+ // - -  - - - - - -  - - - - -- - - - - - - - - - - - - - - - -   on google images:
+
+
+colors = new Array(" #000000 ", "#101010 ","#202020 ", "#282828 ","#303030" ,"#383838  ", "#404040 ", "#484848 ","#585858 " ,"#505050 ", "#606060 ","#686868 ", "#787878 ",  "#888888 ", "#989898 ", "#A0A0A0 ", "#A8A8A8 ","#C0C0C0 ", "#B0B0B0 ", "#F0F0F0 "," #D8D8D8 ", "#F0F0F0 ","#FFFFFF ","#D0D0D0", " #181818 ", "#909090 ")
+
+colorIndex = 0
+idInterval = 0
+
+function choreography_is_the_organization_of_tensions() {
+ document.querySelector("body").style.backgroundColor = colors[colorIndex]
+ colorIndex = (colorIndex + 1) % colors.length
+}
+//setInterval("choreography_is_the_organization_of_tensions()",20)
+
+
+
+var delay="10"; //how many seconds
+var count='0';
+var Texts=new Array();
+Texts[0]="Counterclockwise";
+Texts[1]="Sphere";
+Texts[2]="Spheres";
+Texts[4]="";
+Texts[5]="The private circle";
+Texts[6]="Public sphere";
+Texts[7]="The private circle";
+Texts[8]="";
+Texts[9]="Rotation";
+Texts[10]="";
+Texts[11]="Spinning";
+Texts[12]="Spinning wheel";
+Texts[13]="Perpetual montion";
+Texts[14]="";
+Texts[15]="Repeating itself ";
+Texts[16]="Geometry of time";
+
+    function perpetual_motion(){
+        document.querySelector('#lst-ib').value = Texts[count]; //or: #searchboxinput
+        count++;
+        if(count==Texts.length){count='0';}
+        setTimeout("perpetual_motion()",delay*800);
+    } 
+    // setInterval("perpetual_motion()",1600)
+
+ // - -  - - - - - -  - - - - -- - - - - - - - - - - - - - - - -   
+
 function Thank_You () {
   var win = window.open('curtain_down.html',"_self");
   win.focus();
 }
 
+
+
+/* NEW INPUT ::::: 
+
+function Giro() {
+document.querySelector("p").style.transform="rotate(7deg)"
+}
+setTimeout("Giro()",8000);
+
+document.querySelector(".srg").style.height="70px"
+
+document.querySelector(".srg").style.position="relative"
+
+document.write("www.google.com");
+
+
+var txt = "Hello World!"; 
+document.write("<p>Link: " + txt.link("http://www.w3schools.com") + "</p>");
+
+
+document.querySelector(".img").style.filterGrayscale="100%"
+
+document.querySelector("img").style.filterBlur="15px"
+
+//document.querySelector("#center_col").style.top="100rem" 
+
+//function  mirror() {
+document.querySelector("#search").style.transform = "scale(-1, 1)";
+};  mirror()   
+
+function show() {
+    document.body.style.background = "url(https://media1.giphy.com/media/BPtiK5brjtFpC/200.gif)";}  show()
+*/
+
+// ______________________________________________________ NEW:
+
+// Up and donwn:::::
+
+var xMax, yMax, xNeg=0, yNeg=1;
+
+function pageScroll() {
+    window.scrollBy(5 * xNeg, 5 * yNeg);
+    if(xMax == window.scrollX)xNeg = xNeg * 1;
+    if(yMax == window.scrollY)yNeg = xNeg * -1;
+    scrolldelay = setTimeout(pageScroll,100);
+    console.log(window.scrollY);
+    xMax = window.scrollX;
+    yMax = window.scrollY;
+}
+pageScroll();
+//parar na console: clearTimeout(scrolldelay)
+
+document.querySelector(".srg").style.transform="rotate(180deg)"
 
 
