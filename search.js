17b028c08b56b839d946a8e3f44e32d26cdcb210
JoChicau <JoBCB@users.noreply.github.com>
2019-03-01 13:14:47 +0100

    back up files teatrise on search queries &..

---
+++ search.js
@@ -0,0 +1,209 @@
+ 
+
+<-- TEATRISE ON SEARCH QUERIES & OTHER FORMS OF MOVING -->
+
+
+// music by Keiichiro Shibuya
+
+// search queries: 
+// mirroring bodies;
+// diffraction mirroring bodies;
+// parallel bodies; // parallellism: affinity; correspondence; in between. 
+// layering acts gestures;
+
+// document.getElementById('som').play();
+
+// currentHeadings = Array.from(document.querySelectorAll("h3"))
+// currentHeadings.map(x => console.log(x.innerText))
+
+// var score = document.createElement("p");
+// var new_score = document.createTextNode("this another score");
+// score.appendChild(new_score);
+// document.getElementById("score").appendChild(score);
+
+// scrape text > accumulate > collect a search queries into a new score.
+
+// read seach query:
+
+var delay="10"; 
+var count='0';
+var Texts=new Array();
+Texts[0]="Choreography is ghostly";
+Texts[1]="its presence haunted by absence.";
+Texts[2]="Layers forming and dissolving";
+Texts[4]="scripting a network of relations";
+Texts[5]="like particles all in movement.";
+Texts[6]="The movement that manifests within and without the frame.";
+Texts[7]="";
+Texts[8]="Live performance emphasizes the agency of different elements,";
+Texts[9]="desires, orientations, inclinations, and intensities.";
+Texts[10]="Tools to observe ecosystems,";
+Texts[11]="practices of attention, listening .. breathing.";
+Texts[12]="Framing and scripting";
+Texts[13]="provoking new layouts, alternate relationalities,";
+Texts[14]="and constellations.";
+Texts[15]="Framing and dissolving, how many layers?";
+Texts[16]="";
+Texts[17]="Dance as movement of movements among different bodies,";
+Texts[18]="our bodies, our dances, ourselves.";
+Texts[19]="The body of the dancer becomes this ghostly matter.";
+
+function voicing (){
+        document.querySelector('#text').innerHTML = Texts[count]; 
+        count++;
+        if(count==Texts.length){count='0';}
+        setTimeout("breathe()",delay*800);
+} 
+// setInterval("voicing()",2000)
+// or speaking codes, choreo—graphic codes.
+   
+
+// document.querySelector("body").style.textShadow="0 0 3px #000000";
+// document.querySelector("body").style.border= "1px solid #BFBFBF";
+// document.querySelector("body").style.boxShadow= "10px 10px 5px #aaaaaa";
+
+// document.querySelector("body").style.writingMode="vertical-lr";
+// document.querySelector("body").style.textOrientation="upright";
+// document.querySelector("body").style.lineHeight="150px";
+// document.querySelector("body").style.letterSpacing="150px";
+
+var delay="10"; 
+var count='0';
+var Texts=new Array();
+Texts[0]="Spheres as a counter-social choreography";
+Texts[1]="The private circle";
+Texts[2]="Public sphere";
+Texts[3]="Vicious Circle";
+Texts[4]="Repeating itself";
+Texts[5]="Round and round";
+Texts[6]="Dancing around each other";
+Texts[7]="around language;";
+Texts[8]="around us.";
+
+function  circulation (){
+        document.querySelector('.gLFyf.gsfi').value = Texts[count]; 	
+        //document.querySelector('#text').innerHTML = Texts[count]; 
+        count++;
+        if(count==Texts.length){count='0';}
+        setTimeout("circulation()",delay*800);
+} 
+
+// setInterval("circulation()",2000)
+
+
+function turn () {
+    var centro = document.querySelector("body"),
+    deg = 10;
+    des_centro = setInterval(function() {
+    centro.style.transform = "rotateY(" + deg + "deg)";
+    deg = (deg + 10) % 360
+    }, 80); 
+} 
+// turn ()
+
+
+function pause () {
+    clearInterval(des_centro);
+} 
+// pause ()  
+
+function cirque () {
+        document.querySelector("#appbar").style.display="none";
+        document.querySelector("#top_nav").style.display="none";
+        document.querySelector(".sfbgx").style.display="none";
+        document.querySelector("#searchform").style.display="none";
+        document.querySelector("html").style.backgroundColor="black";    
+        document.querySelector("body").style.border="3px solid black";
+        document.querySelector("body").style.borderRadius="50%";
+        document.querySelector("body").style.width="1200px";
+        document.querySelector("body").style.height="1200px";
+        document.querySelector("body").style.boxShadow= "inset 1em 1em 5.9em black";        
+} 
+
+
+// document.querySelector("#stage01").style.transformY=rotate(180deg);  
+
+
+function accumulation () {
+  window01 = window.open("https://www.google.com/search?q=mirroring+bodies&source=lnt&tbs=qdr:d&sa=X&ved=0ahUKEwie-OD29N7gAhVL1eAKHVC6AGgQpwUIIw&biw=1259&bih=437", height=200, width=1200),
+  window02 = window.open("https://www.google.com/search?q=mirroring+bodies&tbs=qdr:d,sbd:1&source=lnt&sa=X&ved=0ahUKEwjBqPj49N7gAhXnA2MBHRxSA5sQpwUIIw&biw=1259&bih=437", height=1200, width=200),  
+  window03 = window.open("https://www.google.com/search?q=mirroring+bodies&tbs=li:1&source=lnt&sa=X&ved=0ahUKEwiGncWH9d7gAhWH1-AKHdDhDkYQpwUIIw&biw=1259&bih=437", height=900, width=900), 
+  window04 = window.open("https://www.google.com/search?biw=1259&bih=437&tbs=li%3A1&ei=PxV4XMObM6qrgwenz4awBg&q=reflective+bodies+breathing&oq=reflective+bodies+breathing&gs_l=psy-ab.3...51041.58150..58439...0.0..0.82.585.11......0....2j1..gws-wiz.......0i8i7i10i30.uwp7UO7BchU", height=900, width=1200),
+  window05 = window.open("https://www.google.com/search?q=reflective+bodies+breathing&tbs=qdr:h&source=lnt&sa=X&ved=0ahUKEwiF95Gu9d7gAhX56eAKHcuHBx0QpwUIJA&biw=1259&bih=437", height=1200, width=500),
+  window06 = window.open("https://www.google.com/search?q=reflective+bodies+breathing&tbas=0&source=lnt&tbs=sbd:1,qdr:d&sa=X&ved=0ahUKEwichJ3O9d7gAhVa8OAKHWjzDrUQpwUIIw&biw=1259&bih=437", height=600, width=900), 
+  window07 = window.open("https://www.google.com/search?biw=1259&bih=437&tbs=sbd%3A1%2Cqdr%3Ad&ei=9hV4XNHfEIeHjLsPlNuUiA4&q=parallel+bodies&oq=parallel+bodies&gs_l=psy-ab.3..0j0i22i30.326926.326926..327095...0.0..0.49.49.1......0....2j1..gws-wiz.btNFyCPuYOg", height=5200, width=500), 
+  window08 = window.open("https://www.google.com/search?q=parallel+bodies&tbs=qdr:d&tbas=0&source=lnt&sa=X&ved=0ahUKEwj-soaF997gAhX06OAKHY5FAOkQpwUIIw&biw=1259&bih=437", height=200, width=200), 
+  window09 = window.open("https://www.google.com/search?q=parallel+bodies&tbas=0&tbs=li:1&source=lnt&sa=X&ved=0ahUKEwiExJWP997gAhXj1-AKHUCBAQwQpwUIIw&biw=1259&bih=437", height=200, width=50),
+  window10 = window.open("https://www.google.com/search?biw=1259&bih=437&tbs=qdr%3Ad%2Csbd%3A1&ei=thh4XJCHDPedjLsPt-CUuAw&q=parallel+bodies&oq=parallel+bodies&gs_l=psy-ab.3..0j0i22i30.23770.23770..23930...0.0..0.39.39.1......0....2j1..gws-wiz.qNOh_bhEYtQ", height=900, width=600),
+  window11 = window.open("https://www.google.com/search?q=unperformed+potential&source=lnt&tbs=sbd:1,qdr:d&sa=X&ved=0ahUKEwi6hZSA-d7gAhVl2OAKHUckCDEQpwUIIw&biw=1259&bih=437", height=800, width=1800),
+  window12 = window.open("https://www.google.com/search?biw=1259&bih=437&tbs=qdr%3Ad&ei=Bxp4XPKTCIuygwf24p_4Ag&q=framing+and+dissolving&oq=framing+and+dissolving&gs_l=psy-ab.3...58704.58704..59132...0.0..0.80.80.1......0....2j1..gws-wiz.YXGSheDB9Co", height=600, width=800),
+  window13 = window.open("https://www.google.com/search?q=framing+and+dissolving&tbas=0&source=lnt&sa=X&ved=0ahUKEwi-qeaI-t7gAhX3BWMBHV3nDnUQpwUIIw&biw=1259&bih=437", height=500, width=500),
+  window14 = window.open("https://www.google.com/search?q=unperformed+potential&tbas=0&tbas=0&source=lnt&sa=X&ved=0ahUKEwjlm4aQ-d7gAhXYAWMBHYcfBnMQpwUIIw&biw=1259&bih=437", height=50, width=200),
+  window14 = window.open("https://www.google.com/search?biw=1259&bih=437&tbs=qdr%3Ad%2Csbd%3A1&ei=wxl4XMS4Ds6ugwe1iZaIAg&q=alternate+relationalities&oq=alternate+relationalities&gs_l=psy-ab.3...25576.25576..25936...0.0..0.76.76.1......0....2j1..gws-wiz.52Sa14_DrqQ", height=800, width=200),
+  window15 = window.open("https://www.google.com/search?q=alternate+relationalities&tbas=0&source=lnt&sa=X&ved=0ahUKEwjpz7jF-d7gAhWMsBQKHcL4CN4QpwUIIw&biw=1259&bih=437", height=500, width=200)
+}
+
+//accumulation ()
+
+function rupture() {
+    window01.close();
+    window02.close();  
+    window03.close();
+    window04.close();  
+    window05.close();
+    window06.close();
+    window07.close();
+    window08.close();  
+    window09.close();
+    window10.close(); 
+    window11.close();
+    window12.close();
+    window13.close();
+    window14.close();  
+    window15.close();
+}
+
+// unperformed potential:
+
+var delay="10"; 
+var count='0';
+var Texts=new Array();
+
+Texts[0]="";
+Texts[1]="";
+Texts[2]="";
+Texts[4]="";
+Texts[5]="";
+Texts[6]="";
+Texts[7]="";
+Texts[8]="";
+Texts[9]="";
+Texts[10]="";
+Texts[11]="";
+Texts[12]="";
+Texts[13]="";
+Texts[14]="";
+Texts[15]="";
+Texts[16]="";
+Texts[17]="";
+Texts[18]="";
+Texts[19]="" 
+
+function movement_search (){
+        document.querySelector('#logo').innerHTML = Texts[count]; //or: #searchboxinput
+        count++;
+        if(count==Texts.length){count='0';}
+        setTimeout("movement_search()",delay*800);
+
+document.querySelector('#logo').style.fontSize="120pt";
+document.querySelector('#logo').style.width="100cm";
+document.querySelector('body').style.transform="scale(0.5)";
+} 
+
+
+function offstage () {
+    document.body.innerHTML = '';
+    document.head.innerHTML = '';      
+}
+
+// move();
